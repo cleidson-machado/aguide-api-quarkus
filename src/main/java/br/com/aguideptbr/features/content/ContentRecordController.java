d2f86344ca5cfa73af28d47744ffde32a35c7069
Cleidson <cleidson.mac@outlook.com>
2026-02-12 05:53:15 +0000

    fix(content): remove channelName field reference in develop branch
    - Remove channelName field usage from ContentRecordController update method
    - Remove channelName sorting example from documentation comments
    - Fix compilation error: channelName field doesn't exist in develop branch
    - Field will be added when feature/content-channel-id is merged
    - Resolves Jenkins build failure on develop branch

--- src/main/java/br/com/aguideptbr/features/content/ContentRecordController.java
+++ src/main/java/br/com/aguideptbr/features/content/ContentRecordController.java
@@ -49,7 +49,6 @@ public class ContentRecordController {
     // Examples of usage:
     // GET /contents?sort=title&order=asc - Alphabetical order
     // GET /contents?sort=title&order=desc - Descending alphabetical order
-    // GET /contents?sort=channelName&order=asc - By channel
     // GET /contents?sort=publishedAt&order=desc - Most recent published content
     // first
     // GET /contents?page=0&size=10&sort=title&order=asc - Paginated by title
@@ -169,7 +168,6 @@ public class ContentRecordController {
         existing.title = dataFromRequest.title;
         existing.description = dataFromRequest.description;
         existing.videoUrl = dataFromRequest.videoUrl;
-        existing.channelName = dataFromRequest.channelName;
         existing.type = dataFromRequest.type;
         existing.videoThumbnailUrl = dataFromRequest.videoThumbnailUrl;
         existing.setPublishedAt(dataFromRequest.getPublishedAt());
